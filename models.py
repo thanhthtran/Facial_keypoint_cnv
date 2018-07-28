0328bd096246db28e2349f54aa93f0fff6bac3b1
thanhthtran <41564076+thanhthtran@users.noreply.github.com>
2018-07-28 20:15:32 +0300

    Add files via upload

--- models.py
+++ models.py
@@ -15,22 +15,40 @@ class Net(nn.Module):
         
         ## TODO: Define all the layers of this CNN, the only requirements are:
         # Conv lsyer 1-4
+
         self.conv1 = nn.Conv2d(1, 32, 4)
+        nn.init.xavier_uniform_(self.conv1.weight)
+
         self.conv2 = nn.Conv2d(32,64,3)
+        nn.init.xavier_uniform_(self.conv2.weight)
+
         self.conv3 = nn.Conv2d(64,128,2)
+        nn.init.xavier_uniform_(self.conv3.weight)
+
         self.conv4 = nn.Conv2d(128,256,2)
+        nn.init.xavier_uniform_(self.conv4.weight)
 
         #Drop out layer 1-6
         self.drop_out1 = nn.Dropout(p=0.1)
-        self.drop_out2 = nn.Dropout(p=0.2)
-        self.drop_out3 = nn.Dropout(p=0.3)
-        self.drop_out4 = nn.Dropout(p=0.4)
-        self.drop_out5 = nn.Dropout(p=0.4)
-        self.drop_out6 = nn.Dropout(p=0.4)
+        self.drop_out2 = nn.Dropout(p=0.1)
+        self.drop_out3 = nn.Dropout(p=0.2)
+        self.drop_out4 = nn.Dropout(p=0.2)
+        self.drop_out5 = nn.Dropout(p=0.5)
+        self.drop_out6 = nn.Dropout(p=0.5)
+
         # Dense layer 1-3
-        self.fc1 = nn.Linear(12*12*256, 6400)
-        self.fc2 = nn.Linear(6400, 1000)
-        self.fc3 = nn.Linear(1000, 136)
+        self.fc1 = nn.Linear(12*12*256, 3200)
+        nn.init.xavier_uniform_(self.fc1.weight)
+
+        self.fc2 = nn.Linear(3200, 1600)
+        nn.init.xavier_uniform_(self.fc2.weight)
+        
+        self.fc3 = nn.Linear(1600, 2*68)
+        nn.init.xavier_uniform_(self.fc3.weight)
+        
+        
+
+
         # Max pooling layer
         self.pool = nn.MaxPool2d(2, 2)
    
@@ -53,11 +71,11 @@ class Net(nn.Module):
         # Flatten
         x = x.view(x.size(0), -1)
         #Dense
-        x = F.elu(self.fc1(x))
+        x = F.relu(self.fc1(x))
         x = self.drop_out5(x)
         
         x = F.relu(self.fc2(x))
-        x = self.drop_out5(x)
+        x = self.drop_out6(x)
         
         x = self.fc3(x)
     
